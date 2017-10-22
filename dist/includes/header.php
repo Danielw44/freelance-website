f09dc5e4763116c7828d49cad664be7620c72598
Danielw44 <daniel.a.worthington44@gmail.com>
2017-10-22 22:22:12 +0100

    Initial Code Commit

---
+++ dist/includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Daniel Worthington</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="msapplication-TileColor" content="#0A8EFF">
+    <meta name="theme-color" content="#0A8EFF">
+  </head>
+  <body>
+
+  <nav>
+    <ul>
+      <li></li>
+    </ul>
+  </nav>
